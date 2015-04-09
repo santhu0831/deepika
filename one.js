d7d8d0e5fa9c3efd2cb3568db420fda4be1cd69c
santosh <p.santoshkumar16@gmail.com>
2015-04-09 09:56:32 +0530

    added one.js

---
+++ one.js
@@ -0,0 +1,18 @@
+//var x = Math.floor((Math.random() * 10) + 1);
+console.log("Enter the x value");
+console.log("x values is  ",+x);
+var number = 5;
+if(number>x){
+ console.log("Your number is greate then x");
+ console.log("Try to guess anohter number");
+ }
+ else if(number==x)
+{
+  console.log("Your number is equqls to X ",+number,+x);
+}
+
+else
+{
+ console.log("Your number is less than x");
+ console.log("Try to guess anohter number");
+}
